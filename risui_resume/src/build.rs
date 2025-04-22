cd88ccf681dfa3775613b3311121a0c5f1069603
uraneko <BOOTHAlessandra@tutanota.com>
2025-04-22 07:37:25 +0100

    add pandoc build pdf from html

---
+++ risui_resume/src/build.rs
@@ -0,0 +1,26 @@
+use pandoc::{InputFormat, OutputFormat, Pandoc, PandocError, PandocOutput};
+use pandoc::{InputKind, OutputKind, PandocOption};
+
+pub(crate) fn pandoc_build_resume(
+    html: String,
+    output: Option<&str>,
+) -> Result<PandocOutput, PandocError> {
+    let output = if let Some(path) = output {
+        OutputKind::File(path.into())
+    } else {
+        OutputKind::Pipe
+    };
+    let mut pandoc = Pandoc::new();
+    pandoc
+        .set_input(InputKind::Pipe(html))
+        .add_options(&[
+            PandocOption::Standalone,
+            PandocOption::PdfEngine("weasyprint".into()),
+            PandocOption::Css("build.css".into()),
+        ])
+        .set_input_format(InputFormat::Html, vec![])
+        .set_output_format(OutputFormat::Pdf, vec![])
+        .set_output(output);
+
+    pandoc.execute()
+}
